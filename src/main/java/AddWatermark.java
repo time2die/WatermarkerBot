45a8674859c078de7978e107c3f1ef40a49d9d4e
time2die <time2java@gmail.comn>
2020-01-24 18:02:49 +0300

    add new files

---
+++ src/main/java/AddWatermark.java
@@ -0,0 +1,38 @@
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.geom.Rectangle2D;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+public class AddWatermark {
+
+    static void addWatermark(String text, File sourceImageFile, File destImageFile) {
+        try {
+            BufferedImage sourceImage = ImageIO.read(sourceImageFile);
+            Graphics2D g2d = (Graphics2D) sourceImage.getGraphics();
+
+            // initializes necessary graphic properties
+            AlphaComposite alphaChannel = AlphaComposite.getInstance(AlphaComposite.SRC_OVER, 0.1f);
+            g2d.setComposite(alphaChannel);
+            g2d.setColor(Color.BLUE);
+            g2d.setFont(new Font("Arial", Font.BOLD, 64));
+            FontMetrics fontMetrics = g2d.getFontMetrics();
+            Rectangle2D rect = fontMetrics.getStringBounds(text, g2d);
+
+            // calculates the coordinate where the String is painted
+            int centerX = (sourceImage.getWidth() - (int) rect.getWidth()) / 2;
+            int centerY = sourceImage.getHeight() / 2;
+
+            // paints the textual watermark
+            g2d.drawString(text, centerX, centerY);
+
+            ImageIO.write(sourceImage, "png", destImageFile);
+            g2d.dispose();
+
+
+        } catch (IOException ex) {
+            System.err.println(ex);
+        }
+    }
+}
---
+++ src/main/java/WriteImg.java
@@ -0,0 +1,18 @@
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+public class WriteImg {
+
+    static void writeImg(BufferedImage image, String pathname, String formatname) {
+        try {
+            File output_file = new File(pathname);
+            ImageIO.write(image, formatname, output_file);
+            System.out.println("Writing complete.");
+        } catch (
+                IOException e) {
+            System.out.println("Error: " + e);
+        }
+    }
+}
