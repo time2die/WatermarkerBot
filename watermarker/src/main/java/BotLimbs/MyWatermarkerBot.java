bc5632516005e383e0a5d0b685688d08aebbb1e0
mamedvedkov <59172363+mamedvedkov@users.noreply.github.com>
2020-01-25 19:30:44 +0300

    Проверка на длинну сообщений

--- watermarker/src/main/java/BotLimbs/MyWatermarkerBot.java
+++ watermarker/src/main/java/BotLimbs/MyWatermarkerBot.java
@@ -20,19 +20,33 @@ public class MyWatermarkerBot extends TelegramLongPollingBot {
         if (update.getMessage().getChatId().equals(chat_id)) {
         } else {
             if (update.hasMessage() && update.getMessage().hasText()) {
-                SendMessage message = new SendMessage(); // Create a SendMessage object with mandatory fields
-                message.setChatId(chat_id);
-                message.setText(update.getMessage().getDate() + " Cообщение от\n" +
-                        update.getMessage().getChatId() + "\t" +
-                        update.getMessage().getFrom().getFirstName() + "\t" +
-                        update.getMessage().getFrom().getLastName() + "\t" +
-                        update.getMessage().getFrom().getUserName() + "\n" +
-                        update.getMessage().getText());
+                if (update.getMessage().getText().length()<50)
+                {
+                    SendMessage message = new SendMessage(); // Create a SendMessage object with mandatory fields
+                    message.setChatId(update.getMessage().getChatId());
+                    message.setText("Необходимо больше 50 смволов.");
+                    try {
+                        execute(message); // Call method to send the message
+                    } catch (TelegramApiException e) {
+                        e.printStackTrace();
+                    }
+                }
+                else {
+                    SendMessage message = new SendMessage(); // Create a SendMessage object with mandatory fields
+                    message.setChatId(chat_id);
+                    message.setText(update.getMessage().getDate() + " Cообщение от\n" +
+                            "ID\t\tFirstName\t\tLastName\t\tUsername\n" +
+                            update.getMessage().getChatId() + "\t" +
+                            update.getMessage().getFrom().getFirstName() + "\t" +
+                            update.getMessage().getFrom().getLastName() + "\t" +
+                            update.getMessage().getFrom().getUserName() + "\n" +
+                            update.getMessage().getText());
 
-                try {
-                    execute(message); // Call method to send the message
-                } catch (TelegramApiException e) {
-                    e.printStackTrace();
+                    try {
+                        execute(message); // Call method to send the message
+                    } catch (TelegramApiException e) {
+                        e.printStackTrace();
+                    }
                 }
             }
         }
